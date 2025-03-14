6b737ea25734962e64136d52e47309bf9d8d6775
Deep Bikram Thapa Chhetri <deepbikram01@gmail.com>
2025-03-14 21:27:12 +0545

    final update for 3/14

--- server.py
+++ server.py
@@ -3,9 +3,18 @@ import google.generativeai as genai
 import asyncio
 import websockets
 import json
-# Configure Generative AI
-genai.configure(api_key="AIzaSyCUpnaKTGFDjsUzN2ijHobqK-0X0cy_DoY")
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# Get API key from .env file
+api_key = os.getenv("GENAI_API_KEY")
+if not api_key:
+    raise ValueError("API key not found. Please set GENAI_API_KEY in the .env file.")
+
+# Configure Generative AI
+genai.configure(api_key=api_key)
 
 
 new_format_history = []
@@ -52,7 +61,7 @@ process_json_messages(json_filename)
 
 # Set generation config
 generation_config = {
-    "temperature": 2,
+    "temperature": 0.95,
     "top_p": 0.95,
     "top_k": 40,
     "max_output_tokens": 8192,
@@ -61,26 +70,23 @@ generation_config = {
 
 # Create the model
 model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash-exp",
+    model_name="gemini-2.0-flash-exp", 
     generation_config=generation_config,
-    system_instruction="follow the chat history and the converation should be done so taht you should impersonate the history no change of thngs and always try to ask question that inetiate  conversation"
+    system_instruction="follow the chat history and the conversation should be done so that you should impersonate the history no change of things and always try to ask questions that initiate conversation"
 )
 
 # Chat session
 chat_session = model.start_chat(history= new_format_history)
-print(chat_session.history)
 
 async def chat_with_enotes(websocket):
     try:
-        await websocket.send("WebSocket connection established with Enotes!")  # Send welcome message
-
         while True:
             user_input = await websocket.recv()  # Receive input from client
             print(f"User: {user_input}")  # Log user input
 
             # Generate response
             response = chat_session.send_message(user_input)
-            print(f"User: {response}")
+            print(f"User: {response.text}")   
             await websocket.send(response.text)  # Send AI response
 
             # Save chat history
